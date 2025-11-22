d65efe628027a4f0cd3ee62899dd9417162a81aa
Tomas Tamadamas <1058433+tamadamas@users.noreply.github.com>
2025-11-22 22:56:33 +0100

    Basic server

--- server/go/main.go
+++ server/go/main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	router := gin.Default()
+	router.GET("/", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello, World!")
+	})
+
+	if err := router.Run(":3000"); err != nil {
+		fmt.Println("Failed to start server:", err)
+	}
 }
